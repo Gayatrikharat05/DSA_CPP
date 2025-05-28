48d7e62169f273fa824804e7f75a87223816ff51
Gayatri Asaram Kharat <gayatrikharat0505@gmail.com>
2025-05-28 21:23:23 +0530

    "C++ string to Uppercase converter"

---
+++ Touppercase.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+void toupper(char word[],int n){
+    for(int i=0;i<n;i++){
+        char ch=word[i];
+        if(ch>='a'&& ch<='z'){
+            continue;
+        }else{
+           word[i]= ch-'A'+'a';
+        }
+    }
+}
+int main(){
+char word[]="AppLe";
+toupper(word,strlen(word));
+cout<<"Your word was:"<<word; 
+
+    return 0;
+}
